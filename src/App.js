bffbdfd097b8ce2f4e7c783c1d338c9c679faca4
oclivar <oclivar2000@gmail.com>
2023-12-20 17:48:19 -0300

    Actualizar cambios en App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer"
+import { NavBar } from "./components/NavBar/NavBar"
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+       <NavBar />
+       <ItemListContainer greeting= "Este es un mensaje desde la funcion App" />
+      </div>
+      
+    </>
+  )
 }
 
-export default App;
+export default App
