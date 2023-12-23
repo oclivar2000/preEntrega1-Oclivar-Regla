f717274996f3513474e5dcafa2070ed84a97153d
oclivar <oclivar2000@gmail.com>
2023-12-23 00:39:36 -0300

    Agregando el numero 1

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,9 +1,13 @@
 import "./CartWidget.css"
 export const CartWidget = () => {
-    return (
-      <div className="carrito">
-        <p className="tit">CartWidget</p>
-        <p className="carro">🛒</p>
+  const cantidadItems = 1
+  return (
+    <div className="carrito">
+      <p className="tit">CartWidget</p>
+      <h3 className="carro">🛒</h3>
+      <div>
+        <p className="uno">{cantidadItems}</p>
       </div>
-    )
-  }
+    </div>
+  )
+}
