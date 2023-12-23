8e624734b86a72b322884f72ab09cd98f7a907f9
oclivar <oclivar2000@gmail.com>
2023-12-23 18:32:12 -0300

    Ordenando el carrito y el CartWidget

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,10 +2,12 @@ import "./CartWidget.css"
 export const CartWidget = () => {
   const cantidadItems = 1
   return (
-    <div className="carrito">
-      <p className="tit">CartWidget</p>
-      <h3 className="carro">🛒</h3>
-      <div>
+    <div className="carro-y-uno">
+      <div className="carrito">
+        <p className="tit">CartWidget</p>
+        <h3 className="carro">🛒</h3>
+      </div>
+      <div className="carrito">
         <p className="uno">{cantidadItems}</p>
       </div>
     </div>
